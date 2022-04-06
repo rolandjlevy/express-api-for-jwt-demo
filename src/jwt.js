6f292534641c95e9f6b481272324568a78d22efa
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2022-04-06 20:08:52 +0000

    Removed logs

--- src/jwt.js
+++ src/jwt.js
@@ -3,22 +3,18 @@ const secretKey = process.env.TOKEN_SECRET;
 const { statusCode } = require('./utils');
 
 const generateToken = ({ username, customerId }) => {
-  console.log('generateToken');
   return jwt.sign({ username, customerId }, secretKey, { expiresIn: '6h' });
 }
 
 const verifyToken = (req, res, next) => {
-  console.log('///////////// verifyToken called');
   const jwttoken = req.cookies.jwttoken;
   try {
     const data = jwt.verify(jwttoken, secretKey);
     const { username, customerId } = data;
-    console.log('=====================', { data, jwttoken, username, customerId });
     req.username = username;
     req.customerId = customerId;
     next();
   } catch (error) {
-    console.log('///////////// error', error);
     const customError = {
       name: 'Authorization error',
       message: `Only logged-in users can access this page`,
